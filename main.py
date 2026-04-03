d4020544f99efebd36834504863862bfe74596ab
gbauer-baillion <gbauer.baillion@gmail.com>
2026-04-03 21:18:06 +0200

    Draw player

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -14,22 +15,30 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # 1. Fill the background
         screen.fill("black")
 
-        log_state()
+        # 2. Draw objects
+        player.draw(screen)
 
+        # 3. Refresh the screen
         pygame.display.flip()
 
-        # 2. Limit the framerate to 60 FPS and calculate delta time
+        # 4. Handle timing
+        # Limit the framerate to 60 FPS and calculate delta time
         ms_passed = clock.tick(
             60
         )  # .tick(60) returns the milliseconds since the last call
         dt = ms_passed / 1000  # divide by 1000 to convert to seconds
+
+        log_state()
 
 
 if __name__ == "__main__":
